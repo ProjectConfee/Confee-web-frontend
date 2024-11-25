e6a2231669e27070d5aa8168084e45222cb85b06
KeshaliDhananjana <keshdhananjana@gmail.com>
2024-11-25 18:30:21 +0530

    create committee pages

--- src/pages/committee/Author.tsx
+++ src/pages/committee/Author.tsx
@@ -181,9 +181,11 @@ const Authors = () => {
 
             {/* Popup Form */}
             {showPopup && (
-                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center">
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50">
                     <div className="bg-white px-10 py-5 rounded-2xl shadow-lg w-[500px] mt-20">
-                        <h2 className="text-xl font-bold mb-4">{editingMember ? 'Edit Member' : 'Add New Member'}</h2>
+                        <h2 className="text-xl font-bold mb-4">
+                            {editingMember ? 'Edit Member' : 'Add New Member'}
+                        </h2>
                         <form onSubmit={handleSave}>
                             <div className="mb-4">
                                 <label className="block text-sm font-medium mb-1">
