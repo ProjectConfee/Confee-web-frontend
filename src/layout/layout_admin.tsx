dddaecd4d80422a899ba0550bbdd7f4780e8b72d
KeshaliDhananjana <keshdhananjana@gmail.com>
2024-11-25 16:23:59 +0530

    change admin pages

--- src/layout/layout_admin.tsx
+++ src/layout/layout_admin.tsx
@@ -1,8 +1,12 @@
 
 import React, { ReactNode } from 'react';
+import { FaUserCircle } from "react-icons/fa";
+
 import Navbar_admin from '../components/Navbar_admin.tsx';
 
-import { IoMdNotificationsOutline } from "react-icons/io";
+import {
+    IoMdNotificationsOutline
+} from "react-icons/io";
 
 
 interface MainLayoutProps {
@@ -13,11 +17,13 @@ const Layout_admin: React.FC<MainLayoutProps> = ({ children }) => {
 
     return (
         <div className="flex min-h-screen bg-slate-50">
-            <div className="fixed top-0 left-0 h-screen z-10 pt-20"> {/* Add padding-top */}
+            <div className="fixed top-0 left-0 h-screen z-10 pt-16"> {/* Add padding-top */}
                 <Navbar_admin />
             </div>
             <div className="flex flex-col flex-grow">
-                <div className="w-full fixed top-0 left-0 flex items-center justify-between px-6 bg-gradient-to-r from-blue-600 to-blue-900 text-white z-20" style={{ height: '80px' }}>
+                <div className="w-full fixed top-0 left-0 flex items-center justify-between px-6 bg-blue-800 text-white z-20"
+                     style={{ height: '65px' }}
+                >
                     <div className="flex items-center">
                         <img
                             src="src/assets/final%20logo.jpeg"  // replace with your logo path
@@ -32,16 +38,17 @@ const Layout_admin: React.FC<MainLayoutProps> = ({ children }) => {
                         <div className="text-4xl">
                             <IoMdNotificationsOutline />
                         </div>
-                        <div>
-                            <img
-                                src="src/assets/profile.svg"
-                                alt="Profile"
-                                className="w-14 h-14 rounded-full border-2 border-white"
-                            />
+                        <div className="text-4xl">
+                            <FaUserCircle />
+                            {/*<img*/}
+                            {/*    src="src/assets/profile.svg"*/}
+                            {/*    alt="Profile"*/}
+                            {/*    className="w-10 h-10 rounded-full border-2 border-white"*/}
+                            {/*/>*/}
                         </div>
                     </div>
                 </div>
-                <main className="flex-grow pt-20 ml-72"> {/* Adjust padding-top and margin-left to match Navbar */}
+                <main className="flex-grow pt-16 ml-64"> {/* Adjust padding-top and margin-left to match Navbar */}
                     <div className="p-6"> {/* Adjust padding as needed */}
                         {children}
                     </div>
